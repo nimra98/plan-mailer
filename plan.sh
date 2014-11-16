523cbcf97d589e99f990037c16ac57ed0139501c
Garogat <anton.bracke.com@gmail.com>
2014-11-16 18:22:00 +0100

    Create plan.sh

---
+++ plan.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+
+### CONFIG ###
+URL="http://fsg-preetz.de/downloads/protected/vertretungsplan.html"
+CLASS="Q1B"
+TMP="plan.tmp"
+OLD="plan.old"
+SUBJECT="Neuer Eintrag im Vertretungsplan!"
+MAIL="anton.bracke.com@gmail.com"
+##############
+
+wget "$URL" -q -O "$TMP"
+
+ENTRY=$(grep "$CLASS" "$TMP")
+
+if grep -q "$CLASS" "$TMP"
+then
+	echo "found"
+	DAY=$(cat $TMP | grep "<p class=\"Titel\">")
+	DAY=$(echo $DAY | tr -d '\n')
+	echo "day: $DAY"
+	echo "entry: $ENTRY"
+    # code if found
+	if grep -q "$DAY :-:-: $ENTRY" "$OLD"
+	then
+		echo "already mailed!"
+	else
+		echo $ENTRY | mail -s "$SUBJECT" "$MAIL"
+		#wget http://google.de -q -O /dev/null > /dev/null >2 /dev/null
+		echo "$DAY :-:-: $ENTRY" >> "$OLD"
+		echo "mailed new entry!"
+	fi
+fi
+rm "$TMP"
